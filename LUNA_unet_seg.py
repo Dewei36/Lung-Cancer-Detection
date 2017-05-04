066f7a9c2400a662a067835ae53ac163f2f8c97d
zwc662 <zwc662@gmail.com>
2017-05-04 18:13:57 -0400

    Add files via upload
    unet segmentation

---
+++ LUNA_unet_seg.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+
+
+from keras.models import Model
+from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D
+from keras.optimizers import Adam
+from keras.optimizers import SGD
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as K
+import numpy as np
+
+working_path = "/Users/weichaozhou/Documents/Boston University/EC500/DSB3Tutorial/tutorial_code/"
+
+K.set_image_dim_ordering('th')  # Theano dimension ordering in this code
+
+def unet():
+    inputs = Input((1,512, 512))
+    conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(inputs)
+    conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(pool1)
+    conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(pool2)
+    conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(pool3)
+    conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same')(pool4)
+    conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same')(conv5)
+
+    up6 = merge([UpSampling2D(size=(2, 2))(conv5), conv4], mode='concat', concat_axis=1)
+    conv6 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(up6)
+    conv6 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(conv6)
+
+    up7 = merge([UpSampling2D(size=(2, 2))(conv6), conv3], mode='concat', concat_axis=1)
+    conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(up7)
+    conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv7)
+
+    up8 = merge([UpSampling2D(size=(2, 2))(conv7), conv2], mode='concat', concat_axis=1)
+    conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(up8)
+    conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv8)
+
+    up9 = merge([UpSampling2D(size=(2, 2))(conv8), conv1], mode='concat', concat_axis=1)
+    conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(up9)
+    conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv9)
+
+    conv10 = Convolution2D(1, 1, 1, activation='sigmoid')(conv9)
+
+    model = Model(input=inputs, output=conv10)
+
+    model.compile(optimizer=Adam(lr=1.0e-5), loss=loss, metrics=[loss_metrics])
+
+    return model
+
+def loss_metrics(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + 1) / (K.sum(y_true_f) + K.sum(y_pred_f) + 1)
+def loss(y_true, y_pred):
+    return -loss_metrics(y_true, y_pred)
+
+reuse = 0
+train_imgs = np.load(working_path+"trainImages.npy").astype(np.float32)
+train_mask = np.load(working_path+"trainMasks.npy").astype(np.float32)
+test_imgs = np.load(working_path+"testMasks.npy").astype(np.float32)
+test_mask = np.load(working_path+"testMasks.npy").astype(np.float32)
+train_imgs -= np.mean(train_imgs)
+train_imgs /= np.std(train_imgs)
+model = unet()
+model_load= ModelCheckpoint('unet.hdf5', monitor='loss', save_best_only=True)
+if reuse:
+    model.load_weights('./unet.hdf5')
+model.fit(train_imgs, train_mask, batch_size=2, nb_epoch=1, verbose=1, shuffle=True, callbacks=[ModelCheckpoint('unet.hdf5', monitor='loss', save_best_only=True)])
+model.load_weights('./unet.hdf5')
+np.save('masksTestPredicted.npy', test_mask)
+
